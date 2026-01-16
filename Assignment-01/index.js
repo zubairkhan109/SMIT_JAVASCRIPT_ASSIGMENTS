0c13c863f34c2666689e5cc79f977f7ada622ddb
zubairkhan <153979667+zubairkhan109@users.noreply.github.com>
2026-01-17 03:07:45 +0500

    Assignment-01

---
+++ Assignment-01/index.js
@@ -0,0 +1,24 @@
+
+
+// var table = +prompt("Enter Youe Table")
+// var tablelenth = +prompt("Enter Your Table Lenth")
+
+// for(var i = 1; i <= tablelenth; i++){
+//     console.log(`${table} X ${i} = ${table * i}`);
+// }
+
+
+
+
+
+
+var Fullname =["Ahsen","Shayan","Obaid"]
+var lastname =["Rehan","Adeel"]
+                              
+for(var i = 0; i <= Fullname.length; i++){
+            
+    for(var j = 0; j <= lastname.length; j++){
+        console.log(`${Fullname}  ${i}  ${lastname}  ${j}`);
+        
+    }
+}
